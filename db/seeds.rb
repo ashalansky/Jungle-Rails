f2f6b99c165f76dc3a13bf1f16a28717d8e175aa
Amanda Shalansky <ashalansky20@hotmail.com>
2019-09-17 22:18:22 +0000

    added sold badge

--- db/seeds.rb
+++ db/seeds.rb
@@ -55,7 +55,7 @@ cat1.products.create!({
   name:  'Hipster Hat',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('apparel3.jpg'),
-  quantity: 4,
+  quantity: 0,
   price: 34.49
 })
 
@@ -128,7 +128,7 @@ cat3.products.create!({
   name:  'Red Bookshelf',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('furniture3.jpg'),
-  quantity: 23,
+  quantity: 0,
   price: 2_483.75
 })
 
