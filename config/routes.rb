52569d2daf448731a7f0d0705557b3d0931f437b
Amanda Shalansky <ashalansky20@hotmail.com>
2019-09-19 01:44:39 +0000

    adds dynamic sale statuses - active, upcoming, and expired

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:eidt, :update, :show, :destroy]
-    resources :sales, only: [:index]
+    resources :sales, only: [:index, :new]
   end
 
   get '/login'     => 'sessions#new'
